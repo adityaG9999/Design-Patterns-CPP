dc8c65f7b17b4a7872ea63132d661de6ee8e46fe
Aditya Gajare <176593050+adityaG9999@users.noreply.github.com>
2026-04-08 13:00:16 +0530

    Update vehicle_factory.hpp

--- Creational/Factory/include/vehicle_factory.hpp
+++ Creational/Factory/include/vehicle_factory.hpp
@@ -1,1 +1,17 @@
 
+#ifndef vehicle_factory_hpp
+#define vehicle_factory_hpp
+#include<iostream>
+#include <string>
+#include "vehicle.hpp"
+#include "car.hpp"
+#include "bike.hpp"
+using namespace std;
+
+class Vehiclefactory{
+ public : 
+    static Vehicle *getVehicle(string vehicletype);
+};
+
+
+#endif
