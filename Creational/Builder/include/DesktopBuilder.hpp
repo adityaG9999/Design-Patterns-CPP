4c1a87eef7035fcad021df59d0e0a68c8f3e1d7f
Aditya Gajare <176593050+adityaG9999@users.noreply.github.com>
2026-04-08 13:39:37 +0530

    Added desktopbuilder header file

--- Creational/Builder/include/DesktopBuilder.hpp
+++ Creational/Builder/include/DesktopBuilder.hpp
@@ -1,1 +1,24 @@
+#ifndef desktopbuilder_hpp
+#define desktopbuilder_hpp
+#include "desktop.hpp"
 
+class Desktopbuilder{
+protected:
+    Desktop*desktop;
+
+public:
+Desktopbuilder(){
+    desktop=new Desktop();
+};
+
+virtual void buildMonitor()= 0 ;//pure virtual function i.e, classes inheriting this class have to impleent this function 
+virtual void buildKeyboard()=0;
+virtual void buildMouse()=0;
+virtual void buildprocessor()=0;
+virtual void buildram()=0;
+virtual void buildspeaker()=0;
+virtual Desktop*getdesktop(){
+    return desktop;
+}
+};
+#endif
