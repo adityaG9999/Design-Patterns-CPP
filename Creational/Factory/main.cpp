2fadc7adb1d261a5884cf709af74b7bea03a0875
Aditya Gajare <176593050+adityaG9999@users.noreply.github.com>
2026-04-08 13:05:04 +0530

    Update main.cpp

--- Creational/Factory/main.cpp
+++ Creational/Factory/main.cpp
@@ -1,1 +1,14 @@
 
+#include "vehicle_factory.hpp"
+#include <iostream>
+
+using namespace std ; 
+
+int main(){
+    string vehicletype;
+    cin>>vehicletype;
+    Vehicle* vehicle= Vehiclefactory::getVehicle(vehicletype);
+    vehicle-> createVehicle();
+    
+    return 0 ; 
+}
